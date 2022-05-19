9f57096dd0a1ab4bc25ae5e1c75b88effde6ecce
EC2 Default User <ec2-user@ip-172-31-6-132.ap-northeast-1.compute.internal>
2022-05-19 12:39:02 +0000

    add book_comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  get 'books_comments/create'
+  get 'books_comments/destroy'
   get 'favorites/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
---
+++ db/migrate/20220518135927_create_book_comments.rb
@@ -0,0 +1,7 @@
+class CreateBookComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :book_comments do |t|
+      t.timestamps
+    end
+  end
+end
