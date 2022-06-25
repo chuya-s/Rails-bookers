ad2a4eda49828d288dc8d734729ff73565c4317b
EC2 Default User <ec2-user@ip-172-31-6-132.ap-northeast-1.compute.internal>
2022-06-25 06:16:02 +0000

    search function

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,14 @@
+class SearchesController < ApplicationController
+
+  def search
+    @word = params[:word]
+    @range = params[:range]
+    search = params[:search]
+    if @range == '1'
+      @user = User.search(search, @word)
+    else
+      @book = Book.search(search, @word)
+    end
+  end
+
+end
