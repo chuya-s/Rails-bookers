2ee8500f590a7a6116516b38d8a624d882c9f34b
EC2 Default User <ec2-user@ip-172-31-6-132.ap-northeast-1.compute.internal>
2022-06-26 01:04:58 +0000

    book comment: Asynchronize

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,21 +2,15 @@ class BookCommentsController < ApplicationController
   def create
     @bookComment = BookComment.new(book_id:params[:book_id], comment:params[:book_comment])
     @bookComment.user_id = current_user.id
-    if @bookComment.save
-      redirect_to book_path(params[:book_id]), notice: "commented successfully."
-    else
-      redirect_to book_path(params[:book_id]), notice: "commented failed."
-    end
+    @bookComment.save
+    redirect_to request.referer
   end
 
   def destroy
     bookCommentId = params[:book_comment_id]
     @bookComment = BookComment.find(bookCommentId)
-    if @bookComment.delete
-      redirect_to book_path(params[:book_id]), notice: "commented successfully."
-    else
-      redirect_to book_path(params[:book_id]), notice: "commented failed."
-    end
+    @bookComment.delete
+    redirect_to request.referer
   end
 
 
