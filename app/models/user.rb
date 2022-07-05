0a696c82c7490c32a8b957de4b2bbf63425a0072
EC2 Default User <ec2-user@ip-172-31-6-132.ap-northeast-1.compute.internal>
2022-07-05 22:00:25 +0000

    guest login

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,4 +62,10 @@ class User < ApplicationRecord
     end
   end
 
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com', introduction: 'guest users') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 end
